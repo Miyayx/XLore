28c89cbdc6bd6e89bece8ed4b50512ae38e80f4f
Miyayx <yangxue00.yxmn@gmail.com>
2015-03-21 09:52:04 +0800

    断点处理

--- hudong-firstimage-crawler.py
+++ hudong-firstimage-crawler.py
@@ -8,17 +8,39 @@ import os
 import urllib
 from bs4 import BeautifulSoup
 
-fo = open(OUTPUT,'w')
+from subprocess import Popen,PIPE
+
+p = Popen(['tail','-n','1',OUTPUT], stdout=PIPE)
+o,e = p.communicate()
+end = o[0:o.index(' ')]
+print "end-->"+end
+
+fo = open(OUTPUT,'a')
 with open(INSTANCE_LIST) as f:
+    line = f.readline()
+    while True:
+       if end in line:
+           line = f.readline()
+           break
+       line = f.readline()
+    
     line = f.readline()
     while line:
         if '@zh' in line:
-            i = line[0:3]
+            i = line[0:line.index(' ')]
             title = line[line.index('"')+1:line.rindex('"')]
-            page = urllib.urlopen(os.path.join(PREFIX, title)).read()
-            soup = BeautifulSoup(soup)
-            imgurl = soup.find(class_='doc-img').find('img').src
-            fo.write('%s property:hasIcon "%s"@baidu .\n')
-            fo.flush()
+            print i+''+title
+            page = urllib.urlopen(os.path.join(PREFIX, title))
+            soup = BeautifulSoup(page)
+            #print soup.find(class_='doc-img').find('img')
+            imgdoc = soup.find(class_='doc-img')
+            if imgdoc:
+                imgurl = imgdoc.find('img')['src']
+                if not imgurl or len(imgurl) == 0:
+                    continue
+                print imgurl
+                fo.write('%s property:hasIcon "%s"@hudong .\n'%(i,imgurl))
+                fo.flush()
+        line = f.readline()
 fo.close()
             
