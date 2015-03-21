566414ba81bd0444beb051dde34b028256757f40
Miyayx <yangxue00.yxmn@gmail.com>
2015-03-21 10:22:37 +0800

    add threading for hudong firstimage

--- hudong-firstimage-crawler.py
+++ hudong-firstimage-crawler.py
@@ -9,6 +9,8 @@ import urllib
 from bs4 import BeautifulSoup
 
 from subprocess import Popen,PIPE
+import threading
+import time
 
 p = Popen(['tail','-n','1',OUTPUT], stdout=PIPE)
 o,e = p.communicate()
@@ -16,6 +18,25 @@ end = o[0:o.index(' ')]
 print "end-->"+end
 
 fo = open(OUTPUT,'a')
+mutex = threading.Lock()
+
+def crawl(i, title):
+    global mutex
+
+    page = urllib.urlopen(os.path.join(PREFIX, title))
+    soup = BeautifulSoup(page)
+    #print soup.find(class_='doc-img').find('img')
+    imgdoc = soup.find(class_='doc-img')
+    if imgdoc:
+        imgurl = imgdoc.find('img')['src']
+        if not imgurl or len(imgurl) == 0:
+            return
+        print imgurl
+        mutex.acquire()
+        fo.write('%s property:hasIcon "%s"@hudong .\n'%(i,imgurl))
+        fo.flush()
+        mutex.release()
+
 with open(INSTANCE_LIST) as f:
     line = f.readline()
     while True:
@@ -30,17 +51,13 @@ with open(INSTANCE_LIST) as f:
             i = line[0:line.index(' ')]
             title = line[line.index('"')+1:line.rindex('"')]
             print i+''+title
-            page = urllib.urlopen(os.path.join(PREFIX, title))
-            soup = BeautifulSoup(page)
-            #print soup.find(class_='doc-img').find('img')
-            imgdoc = soup.find(class_='doc-img')
-            if imgdoc:
-                imgurl = imgdoc.find('img')['src']
-                if not imgurl or len(imgurl) == 0:
-                    continue
-                print imgurl
-                fo.write('%s property:hasIcon "%s"@hudong .\n'%(i,imgurl))
-                fo.flush()
+            t = threading.Thread(target=crawl, args=(i,title))
+            t.start()
+            t.join(8)
+            if t.is_alive():
+                print "timeout alive"
+                line = f.readline()
+                continue
         line = f.readline()
 fo.close()
             
